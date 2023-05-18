fa696c496983d1d75c512cc5835516944e22495f
Rehana Abdulber <rabdulbe@its.jnj.com>
2023-05-18 14:29:20 +0300

    Initial Commit

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath : './apps/orders/.env',
+    }),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
