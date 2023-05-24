6c3c829627822499073cda8070dfd4430572b71a
Rehana Abdulber <rabdulbe@its.jnj.com>
2023-05-24 11:59:47 +0300

    added common library

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -3,6 +3,10 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
   imports: [
@@ -11,10 +15,12 @@ import { ConfigModule } from '@nestjs/config';
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
       }),
-      envFilePath : './apps/orders/.env',
+      envFilePath: './apps/orders/.env',
     }),
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
