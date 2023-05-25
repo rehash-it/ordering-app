caed14e45f3845adbc4b2153b0f2fb54a588000f
Rehana Abdulber <rabdulbe@its.jnj.com>
2023-05-25 11:47:26 +0300

    added session transaction for create order event

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
-  createOrder(request: CreateOrderRequest) {
-    return this.ordersRepository.create(request);
+  async createOrder(request: CreateOrderRequest) {
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+      await lastValueFrom(this.billingClient.emit('order_created', order));
+      await session.commitTransaction();
+      return order;
+    } catch (err) {
+      session.abortTransaction();
+      throw err;
+    }
   }
 
   getOrders() {
